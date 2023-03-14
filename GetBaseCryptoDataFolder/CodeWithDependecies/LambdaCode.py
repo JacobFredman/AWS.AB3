99530924082d510961d06d7ce04d287d459f1f9d
“Jacob <jacov141@gmail.com>
2023-03-14 10:03:33 +0200

    adding lambda code in GetBaseCryptoDataFolder

---
+++ GetBaseCryptoDataFolder/CodeWithDependecies/LambdaCode.py
@@ -0,0 +1,30 @@
+import boto3
+import requests
+import json
+
+
+
+
+def xx(event, context):
+    # API call
+    response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    data = json.loads(response.text)
+
+    # DynamoDB setup
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('cryptoTable')
+
+    # Save data to DynamoDB
+    table.put_item(
+        Item={
+            'Id': data['id'],
+            'userId': data['userId'],
+            'title': data['title'],
+            'completed': data['completed']
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Data saved to DynamoDB')
+    }
